f4f8665f0b667f64edbb1186dffa5e87fc1a5d6c
tivmzk <ost0108@naver.com>
2022-01-25 16:41:01 +0900

    faq list

--- src/main/java/kr/co/washing/controller/RootController.java
+++ src/main/java/kr/co/washing/controller/RootController.java
@@ -1,17 +1,25 @@
 package kr.co.washing.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import kr.co.washing.model.Faq;
 import kr.co.washing.model.Subscription;
+import kr.co.washing.service.FaqService;
 import kr.co.washing.service.SubscriptionService;
+import kr.co.washing.util.Pager;
 
 @Controller
 public class RootController {
 	@Autowired
 	SubscriptionService ss;
+	@Autowired
+	FaqService fs;
 	
 	@GetMapping("/")
 	public String index() {
@@ -30,7 +38,9 @@ public class RootController {
 	}
 	
 	@GetMapping("/faq")
-	public String faq() {
+	public String faq(Model model,Pager pager) {
+		List<Faq> list = fs.list(pager);
+		model.addAttribute("list", list);
 		return "faq.main";
 	}
 }
--- src/main/webapp/WEB-INF/views/faq.jsp
+++ src/main/webapp/WEB-INF/views/faq.jsp
@@ -1,2 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<div>
+	<div class="bound">
+		<h5>자주 묻는 질문</h5>
+		<h1>무엇을 도와드릴까요?</h1>
+		<div>
+			<input type="text" />
+			<img src="/re/svg/search.svg"/>
+		</div>
+	</div>
+	<div class="bound">
+		<h2>정보</h2>
+		<ul>
+			<c:forEach items="${list}" var="item">
+				<li>
+					<div>
+						<h3>${item.title}</h3>
+						<img src="/re/svg/drop_down.svg"/>
+					</div>
+					<div>
+						<p>${item.contents}</p>
+					</div>
+				</li>	
+			</c:forEach>		
+		</ul>
+	</div>
+</div>
